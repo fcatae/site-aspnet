20806e8311ad5ac305a332a6dcb6f40cec81268f
Fabricio Catae <fcatae@hotmail.com>
2020-01-29 14:18:18 -0300

    mudanca

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace fur
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Ola mundo");
+                    await context.Response.WriteAsync("Boa tarde");
                 });
             });
         }
