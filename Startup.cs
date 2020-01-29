0ec2775f17102a993968ff6ee256d80017a916f1
Fabricio Catae <fcatae@hotmail.com>
2020-01-29 14:11:30 -0300

    ola mundo

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace fur
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Ola mundo");
                 });
             });
         }
